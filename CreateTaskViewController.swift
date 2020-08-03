54159bfa2c3d7c785b49966ac2510ac1edecf6ec
Ben Leimbach <benleimbach@gmail.com>
2020-08-02 23:19:07 -0600

    added code for the second screen storyboard

---
+++ CreateTaskViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Ben Leimbach on 8/2/20.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwith: UISwitch!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var addTapped: NSLayoutConstraint!
+    
+ 
+}
